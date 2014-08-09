0a5e0b849ffb4d0921aae1eeabc20240b82b5869
黄文鉴 <concefly@foxmail.com>
2014-08-08 23:47:45 -0700

    Debug buf_dev.ok!

---
+++ kernel_module/chr/buf-misc/.tmp_versions/buf_dev.mod
@@ -0,0 +1,2 @@
+/home/concefly/project/git/openRPi/gpio/kernel_module/chr/buf-misc/buf_dev.ko
+/home/concefly/project/git/openRPi/gpio/kernel_module/chr/buf-misc/buf_dev.o
--- kernel_module/chr/buf-misc/buf_dev.c
+++ kernel_module/chr/buf-misc/buf_dev.c
@@ -3,6 +3,7 @@
 #include <linux/fs.h>
 #include <linux/miscdevice.h>
 #include <linux/uaccess.h>
+#include <linux/slab.h>
 #include <linux/errno.h>
 
 /* 所有混杂设备的主设备号都为10
@@ -10,35 +11,66 @@
  * 设备目录 /dev/buf_dev
  */
 
-static char buf_array[50];
+#define BUF_SIZE 50
 
-static int buf_dev_open(struct inode * ip, struct file * fp)
+static int buf_dev_open(struct inode * inode, struct file * filp)
 {
+	char * buf_array=NULL;
+	buf_array = kmalloc(BUF_SIZE,GFP_KERNEL);
+	if(!buf_array)
+		return -EINVAL;
+	filp->private_data = buf_array;
+
 	printk(KERN_INFO "buf_dev open\n");
 	return 0;
 }
 
-static int buf_dev_release(struct inode * ip, struct file * fp)
+static int buf_dev_release(struct inode * inode, struct file * filp)
 {
+	kfree(filp->private_data);
 	printk(KERN_INFO "buf_dev release\n");
 	return 0;
 }
 
 static ssize_t buf_dev_read(struct file * filp, char __user * up, size_t size, loff_t * off)
 {
-	int min = (sizeof(buf_array) - filp->f_ops) - off < size ? : size ;
-	if(copy_to_user(up, buf_array + filp->f_ops + off, min))
+	char * buf_array=NULL;
+	int min=0;
+
+	printk(KERN_INFO "------------buf_dev read------------\n");
+	printk(KERN_INFO "size=%d, off=%d\n",size,*off);
+	
+	buf_array = filp->private_data;
+	min = BUF_SIZE - *off < size? BUF_SIZE - *off:size;
+
+	if(copy_to_user(up, &(buf_array[*off]), min))
 		return -EINVAL;
-	printk(KERN_INFO "buf_dev read. request_size=%d, request_off=%d, f_ops=%d, real_readsize=%d\n",size,off,filp->f_ops,min);
+	*off += min;
+	
+	printk(KERN_INFO "real_read_size=%d\n",min);
+	printk(KERN_INFO "-------------------------------------\n");
+
 	return min;
 }
 
 static ssize_t buf_dev_write(struct file * filp, const char __user * up, size_t size, loff_t * off)
 {
-	int min = (sizeof(buf_array) - filp->f_ops) - off < size ? : size ;
-	if(copy_from_user(buf_array + filp->f_ops, up, min))
+	char * buf_array=NULL;
+	int min=0;
+	
+	printk(KERN_INFO "------------buf_dev write------------\n");
+	printk(KERN_INFO "size=%d, off=%d\n",size,*off);
+
+	buf_array = filp->private_data;
+	min = BUF_SIZE - *off < size? BUF_SIZE - *off:size;
+
+	if(copy_from_user(&(buf_array[*off]), up, min))
 		return -EINVAL;
-	printk(KERN_INFO "buf_dev write. request_size=%d, request_off=%d, f_ops=%d, real_writesize=%d\n",size,off,filp->f_ops,min);
+	*off += min;
+
+	printk(KERN_INFO "real_write_size=%d\n",min);
+	printk(KERN_INFO "-------------------------------------\n");
+
 	return min;
 }
 
@@ -57,13 +89,13 @@ static loff_t bur_dev_llseek(struct file * filp, loff_t off, int where)
 			break;
 
 		case 2: /* SEEK_END */
-			newpos = sizeof(buf_array) + off;
+			newpos = BUF_SIZE + off;
 			break;
 
 		default :
 			return -EINVAL;
 	}
-	if (newpos < 0 || newpos > sizeof(buf_array)-1)
+	if (newpos < 0 || newpos > BUF_SIZE-1)
 		return -EINVAL;
 
 	filp->f_pos = newpos;
@@ -71,7 +103,7 @@ static loff_t bur_dev_llseek(struct file * filp, loff_t off, int where)
 	return newpos;
 }
 
-struct file_operations buf_dev_fops =
+static struct file_operations buf_dev_fops =
 {
 	.owner = THIS_MODULE,
 	.open = buf_dev_open,
@@ -90,6 +122,7 @@ static struct miscdevice buf_dev =
 
 static int __init buf_dev_init(void)
 {
+	
 	misc_register(&buf_dev);
 	printk(KERN_INFO "buf_dev init\n");
 	return 0;
