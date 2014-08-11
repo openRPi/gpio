b466531707a7ca2effc8fbe87f5df2716baba1f7
黄文鉴 <concefly@foxmail.com>
2014-08-11 10:54:41 +0800

    添加 blink 和 button 的代码注释

--- basic/button/value_display/button.c
+++ basic/button/value_display/button.c
@@ -1,14 +1,29 @@
+/**
+ * 简单的检测指定GPIO输入电平的函数库
+ * 
+ * Copyright (C) 2014 concefly <h.wenjian@openrpi.org>
+ * Copyright (C) 2014 openRPi
+ *
+ *	代码遵循GNU协议
+ *
+ * 文档：http://www.openrpi.org/blogs/?p=232
+ */
 #include "button.h"
 #include "bcm2835.h"
 
 #define BTN_PIN		RPI_V2_GPIO_P1_11
 
+/**
+ * GPIO初始化，按键初始化
+ * @return: 成功1，失败0
+ */
 int button_init(void)
 {
 	if(!bcm2835_init())
 		return 0;
 	bcm2835_gpio_fsel(BTN_PIN, BCM2835_GPIO_FSEL_INPT);
-	// with a pullup
+	
+	// 设置GPIO下拉电阻
 	bcm2835_gpio_set_pud(BTN_PIN, BCM2835_GPIO_PUD_DOWN);
 	return 1;
 }
