b6bd1f90b9818c9b180fed9f7613b3ee5b98b0e2
黄文鉴 <concefly@foxmail.com>
2014-08-29 11:04:19 +0800

    chr_btn_dev：映射8个GPIO到中断

--- kernel_module/chr/btn-misc/chr_btn_dev.c
+++ kernel_module/chr/btn-misc/chr_btn_dev.c
@@ -19,48 +19,35 @@
 #include <linux/gpio.h>
 #include <linux/interrupt.h>
 
-#define BTN_NUM	17
+static int btn_list[] = {4,17,18,27,22,23,24,25};
 
 static irqreturn_t btn_interrput(int irq, void *dev)
 {
 	printk(KERN_INFO "++ btn_interrput ++\n");
-	printk(KERN_INFO "irq=%d \n",irq);
+	printk(KERN_INFO "IRQ=%d, GPIO_NUM=%d\n",irq, irq_to_gpio(irq));
 	printk(KERN_INFO "-- btn_interrput --\n");
 	return IRQ_HANDLED;
 }
 
-struct file_operations chr_btn_dev_fops =
-{
-	.owner = THIS_MODULE,
-};
-
-static struct miscdevice chr_btn_dev =
-{
-	.minor = MISC_DYNAMIC_MINOR,
-	.name = "chr_btn_dev",
-	.fops = &chr_btn_dev_fops,
-};
-
 static int __init chr_btn_dev_init(void)
 {
-	int btn_irq;
-
 	printk(KERN_INFO "++ chr_btn_dev init ++\n");
 
-	btn_irq = gpio_to_irq(BTN_NUM);
-
-	if(request_irq(	btn_irq,
-					btn_interrput,
-					IRQF_TRIGGER_RISING,
-					"chr_btn_dev",
-					NULL))
+	for(int i=0;i<ARRAY_SIZE(btn_list);i++)
 	{
-		printk(KERN_ERR "Can't register IRQ %d\n",btn_irq);
-		return -EIO;
+		int btn_irq = gpio_to_irq(btn_list[i]);
+		if(request_irq(	btn_irq,
+						btn_interrput,
+						IRQF_TRIGGER_RISING,
+						"chr_btn_dev",
+						NULL))
+		{
+			printk(KERN_ERR "Can't register IRQ %d\n",btn_irq);
+			return -EIO;
+		}
+		else
+			printk(KERN_ERR "Register IRQ %d\n",btn_irq);
 	}
-	printk(KERN_INFO "register IRQ %d\n",btn_irq);
-
-	misc_register(&chr_btn_dev);
 
 	printk(KERN_INFO "-- chr_btn_dev init --\n");
 	return 0;
@@ -72,12 +59,12 @@ static void __exit chr_btn_dev_exit(void)
 
 	printk(KERN_INFO "++ chr_btn_dev exit ++\n");
 
-	btn_irq = gpio_to_irq(BTN_NUM);
-
-	free_irq(btn_irq,NULL);
-	printk(KERN_INFO "release IRQ %d\n",btn_irq);
-
-	misc_deregister(&chr_btn_dev);
+	for(int i=0;i<ARRAY_SIZE(btn_list);i++)
+	{
+		int btn_irq = gpio_to_irq(btn_list[i]);
+		free_irq(btn_irq,NULL);
+		printk(KERN_INFO "release IRQ %d\n",btn_irq);
+	}
 
 	printk(KERN_INFO "-- chr_btn_dev exit --\n");
 }
