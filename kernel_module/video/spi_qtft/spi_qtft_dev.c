f56a905301c2848814db2b168825cf3c7dc3ceea
黄文鉴 <concefly@foxmail.com>
2014-09-01 13:41:35 +0800

    完成大体框架，但依然导致卸载时死机。

--- kernel_module/video/spi_qtft/spi_qtft_dev.c
+++ kernel_module/video/spi_qtft/spi_qtft_dev.c
@@ -21,20 +21,82 @@
 #define func_in()	printk(KERN_INFO "++ %s:%s (%d) ++\n", __FILE__, __func__, __LINE__)
 #define func_out()	printk(KERN_INFO "-- %s:%s (%d) --\n", __FILE__, __func__, __LINE__)
 
-static struct platform_device spi_qtft_pdev
+static struct platform_device spi_qtft_device = 
 {
-	.name = "spi_qtft_dev",
+	.name = "spi_qtft",
 	.id   = 0,
 };
 
-static struct fb_ops spi_qtft_dev_ops = 
+static struct fb_ops spi_qtft_ops = 
 {
 	.owner = THIS_MODULE,
 };
 
-static int __init spi_qtft_dev_probe(struct platform_device * pdev)
+static int __init spi_qtft_probe(struct platform_device * pdev)
 {
 	struct fb_info *info;
+	func_in();
 
-	
+	info = framebuffer_alloc(0, &pdev->dev);
+	// initialize_fb_info(info, pdev);
+	info->fbops = &spi_qtft_ops;
+	register_framebuffer(info);
+
+	platform_set_drvdata(pdev,info);
+
+	func_out();
+	return 0;
 }
+
+static int spi_qtft_remove(struct platform_device *pdev)
+{
+	struct fb_info *info;
+	func_in();
+
+	info = platform_get_drvdata(pdev);
+	unregister_framebuffer(info);
+	framebuffer_release(info);
+	platform_set_drvdata(pdev,NULL);
+
+	func_out();
+	return 0;
+}
+
+static struct platform_driver spi_qtft_driver = 
+{
+	.probe = spi_qtft_probe,
+	.remove = spi_qtft_remove,
+	.driver = 
+	{
+		.name = "spi_qtft_driver",
+	},
+};
+
+static int  __init spi_qtft_init(void)
+{
+	int err;
+	func_in();
+
+	platform_device_add(&spi_qtft_device);
+	err = platform_driver_register(&spi_qtft_driver);
+
+	func_out();
+	return err;
+}
+
+static void __init spi_qtft_exit(void)
+{
+	func_in();
+
+	platform_driver_unregister(&spi_qtft_driver);
+	platform_device_unregister(&spi_qtft_device);
+
+	func_out();
+}
+
+module_init(spi_qtft_init);
+module_exit(spi_qtft_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("h.wenjian@openrpi.org");
+MODULE_DESCRIPTION("A spi_qtft");
